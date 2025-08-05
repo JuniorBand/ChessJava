9631b7f7c3a8c08ea310c9d412e005907d636a41
Júnior Bandeira <bandeirajunior150@gmail.com>
2025-08-04 22:11:32 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,10 +2,9 @@ package boardgame;
 
 import lombok.Getter;
 
-public class Piece {
+public abstract class Piece {
 
     protected Position position;
-
     private Board board;
 
     public Piece(Board board){
@@ -17,4 +16,21 @@ public class Piece {
         return board;
     }
 
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] moves = possibleMoves();
+        for (int i = 0; i < moves.length; i++) {
+            for (int j = 0; j < moves[i].length; j++) {
+                if (moves[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
